1ead0c82872bccd0bdb96b859e857809957e25d0
SR Tamim <saifur.tamim@gmail.com>
2022-08-20 07:39:14 +0600

    tasks saved in localstorage

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import SingleTask from './components/SingleTask/SingleTask';
 
 function App() {
   const taskInput = useRef(null);
-  const [tasks, setTasks] = useState(null)
-  const [tasksLoading, setTasksLoading] = useState(true)
+  const [tasks, setTasks] = useState([])
+  console.log(tasks)
 
-  // sort tasks to show new task on top
-  tasks && tasks.sort((a, b) => b.timeStamp - a.timeStamp)
 
   useEffect(loadTasks, [])
   // load all tasks function
   function loadTasks() {
-    fetch('https://to-do-srt.herokuapp.com/tasks')
-      .then(res => res.json())
-      .then(data => setTasks(data))
-      .finally(() => setTasksLoading(false))
+    const savedTasksJson = localStorage.getItem('myTasks')
+    if (!savedTasksJson) return
+
+    const savedTasks = JSON.parse(savedTasksJson)
+    // sort tasks to show new task on top
+    savedTasks.sort((a, b) => b.taskAddedTime - a.taskAddedTime)
+
+    setTasks(savedTasks)
+  }
+  function saveTasks(newData) {
+    localStorage.setItem('myTasks', JSON.stringify(newData))
   }
 
   // add new task function
@@ -31,50 +36,31 @@ function App() {
     }
 
     const newTask = {
-      timeStamp: Date.now(),
+      taskAddedTime: Date.now(),
       toDo: taskInput.current.value,
       taskStatus: 'pending'
     }
-    // send new task to server
-    fetch('https://to-do-srt.herokuapp.com/tasks', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newTask)
-    })
-      .then(res => res.json())
-      .then(data => {
-        if (data.insertedId) {
-          e.target.reset()
-          loadTasks()
-        }
-      })
+
+    // save task in localstorage
+    saveTasks([...tasks, newTask])
+    e.target.reset()
+    loadTasks()
   }
 
   // delete any task fron task list
-  function deleteTask(timeStamp) {
-    fetch('https://to-do-srt.herokuapp.com/tasks', {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ timeStamp })
-    })
-      .then(res => res.json())
-      .then(data => data.deletedCount === 1 && loadTasks())
+  function deleteTask(taskAddedTime) {
+    saveTasks(tasks.filter(task => task.taskAddedTime !== taskAddedTime))
+    loadTasks()
   }
 
   // change any state of any task
   function changeTaskState(changedTask) {
-    fetch('https://to-do-srt.herokuapp.com/tasks', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(changedTask)
-    }).then(res => res.json())
-      .then(data => data.modifiedCount && loadTasks())
+    const newData = [
+      ...tasks.filter(task => task.taskAddedTime !== changedTask.taskAddedTime),
+      { ...changedTask, taskDoneTime: Date.now() }
+    ]
+    saveTasks(newData)
+    loadTasks()
   }
 
   return (
@@ -95,14 +81,10 @@ function App() {
             className='tw-font-bold tw-text-primary tw-cursor-pointer tw-py-1 tw-border-primary tw-border-4 tw-border-double hover_tw-bg-red-50' />
         </form>
       </div>
-      {tasksLoading ? <p className='tw-text-4xl tw-text-center tw-my-24'>Loading...</p> :
-        <div id="allTasks">
-          {tasks &&
-            tasks.map((task, i) => <SingleTask sl={i + 1} task={task}
-              functions={{ deleteTask, changeTaskState }} key={i} />)
-          }
-        </div>
-      }
+      <div id="allTasks">
+        {tasks.map((task, i) => <SingleTask task={task}
+          functions={{ deleteTask, changeTaskState }} key={i} />)}
+      </div>
     </div>
   );
 }
