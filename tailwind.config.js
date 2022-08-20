550d7cde8174b289fdaf5d9de972a26d79ffc4fc
SR Tamim <saifur.tamim@gmail.com>
2022-08-20 08:56:22 +0600

    material tailwind installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
-module.exports = {
+const withMaterialUI = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMaterialUI({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: 'darkred',
+        primary: 'darkgreen',
+        primaryHover: '#00990022',
       }
     },
   },
-  plugins: [],
-  prefix: 'tw-',
-  separator: '_'
-}
+  plugins: []
+})
