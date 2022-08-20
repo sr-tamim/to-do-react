99c57049ae84263169cd9f0340f0c9cbf73b96c5
SR Tamim <saifur.tamim@gmail.com>
2022-08-20 08:57:22 +0600

    task card improvements

--- src/components/SingleTask/SingleTask.js
+++ src/components/SingleTask/SingleTask.js
@@ -1,26 +1,37 @@
 import React from 'react';
+import './SingleTask.css'
 
-const SingleTask = ({ sl, task, functions }) => {
+const SingleTask = ({ task, functions }) => {
     const { deleteTask, changeTaskState } = functions
 
-    const taskAddedTime = new Date(task.timeStamp)
+    const taskAddedTime = new Date(task.taskAddedTime)
+    const taskDoneTime = new Date(task.taskDoneTime)
     return (
-        <div className='tw-my-6'>
-            <i>{sl}</i>
-            <h4 className={`tw-font-bold tw-text-xl ${task.taskStatus === 'done' && 'tw-line-through'}`}>{task.toDo}</h4>
-            <h6 className='tw-text-md'>{task.taskStatus}</h6>
-            <time className='tw-text-gray-500 tw-text-xs'>
-                Added at {taskAddedTime.toLocaleString()}
-            </time> <br />
+        <div className='my-6'>
+            <div>
+                <input type="checkbox" />
+            </div>
+            <h4 className={`font-bold text-xl ${task.taskStatus === 'done' && 'line-through'}`}>{task.toDo}</h4>
+
+            <div className="taskInformations">
+                <h6 className='text-md'>{task.taskStatus}</h6>
+                <time className="text-gray-500 text-xs after:content-['*'] after:ml-0.5 after:text-red-500">
+                    Created at {taskAddedTime.toLocaleString()}
+                </time>
+                {task.taskDoneTime &&
+                    <time className='text-gray-500 text-xs'>
+                        Done at {taskDoneTime.toLocaleString()}
+                    </time>}
+            </div>
             <button
-                className='tw-text-primary tw-border-primary hover_tw-bg-red-100 tw-border-2 tw-px-4 tw-my-2 tw-rounded-md'
+                className='text-primary border-primary hover_bg-red-100 border-2 px-4 my-2 rounded-md'
                 onClick={() => changeTaskState({ ...task, taskStatus: 'done' })}>
                 Done
             </button>
 
             <button
-                className='tw-text-primary tw-border-primary hover_tw-bg-red-100 tw-border-2 tw-px-4 tw-m-2 tw-rounded-md'
-                onClick={() => deleteTask(task.timeStamp)}>
+                className='text-primary border-primary hover_bg-red-100 border-2 px-4 m-2 rounded-md'
+                onClick={() => deleteTask(task.taskAddedTime)}>
                 Delete
             </button>
         </div>
