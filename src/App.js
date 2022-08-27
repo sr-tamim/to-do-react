d2f92e8d82c776508818cde549f82605ea4620d6
SR Tamim <saifur.tamim@gmail.com>
2022-08-27 07:25:05 +0600

    warning solve

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
   })
   useEffect(() => {
     user && loadTasksFromServer(user.email)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user])
 
   function logOut() {
